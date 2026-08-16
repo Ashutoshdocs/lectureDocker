abe2200307f74cd7bf16a2c04967e5fc02cb0740
Friend User <friend@gmail.com>
2026-08-16 17:02:52 +0530

    test

---
+++ DockerfileLearning/mysql-demo/init.sql
@@ -0,0 +1,16 @@
+﻿CREATE DATABASE IF NOT EXISTS trainingdb;
+
+USE trainingdb;
+
+CREATE TABLE IF NOT EXISTS students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    course VARCHAR(100)
+);
+
+INSERT INTO students (name, course)
+VALUES
+    ('Student01', 'Docker'),
+    ('Student02', 'Kubernetes'),
+    ('Student03', 'Azure');
+
